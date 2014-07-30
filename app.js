1891fb4bd80d428f738805b6d1395456712daf8c
benschenker <binsch@gmail.com>
2014-07-30 00:21:30 -0400

    changed port back to original

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ var week = day * 7;
  * Express configuration.
  */
 
-app.set('port', process.env.DEV_PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(compress());
